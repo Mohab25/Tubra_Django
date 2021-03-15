9551fc1306e2a9a1301f470029b89100d557b10a
mohab <homab3@gmail.com>
2021-03-15 20:23:24 +0200

    remove token from bash script

--- gitter.sh
+++ gitter.sh
@@ -1,11 +1,10 @@
 # !/bin/bash
 clear
 echo "automating git process"
-git branch -d main 
 git branch main
 git checkout main
 git push origin main
 git checkout master
 git branch -d main
 echo "end successfully !"
- 
+
