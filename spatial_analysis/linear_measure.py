f2f3af964ee8d94f5470b844456d5c818a424bf5
mohab <homab3@gmail.com>
2021-03-26 11:56:58 +0200

    setting up the linear measurement api end point

--- spatial_analysis/linear_measure.py
+++ spatial_analysis/linear_measure.py
@@ -5,19 +5,23 @@ def make_linear_measure(coords_1,coords_2):
     # create two points from the coords
     with connection.cursor() as cur:
         p1,p2,d='','',''
-        coords_1_lng = coords_1[1]
         coords_1_lat = coords_1[0]
-        coords_2_lng = coords_2[1]
+        coords_1_lng = coords_1[1]
         coords_2_lat = coords_2[0]
+        coords_2_lng = coords_2[1]
+    
+    # postgres expects lng,lat in it's functions
 
-        cur.execute(f'select ST_AsText(ST_SetSRID(ST_MakePoint({coords_1_lng},{coords_1_lat}),4326))')
+        cur.execute(f'select ST_GeomFromText(ST_AsText(ST_SetSRID(ST_MakePoint({coords_1_lng},{coords_1_lat}),3785)),3785)')
         for i in cur:
+            print('full cursor:',i)
             p1=i[0]
             print('point#1 - ',p1)
-        cur.execute(f'select ST_AsText(ST_SetSRID(ST_MakePoint({coords_2_lng},{coords_2_lat}),4326))')
+        cur.execute(f'select ST_GeomFromText(ST_AsText(ST_SetSRID(ST_MakePoint({coords_2_lng},{coords_2_lat}),3785)),3785)')
         for i in cur:
             p2=i[0]
-        cur.execute(f"select ST_Distance(ST_GeomFromText('{p1}'),ST_GeomFromText('{p2}'))")
+            print('point#2 - ',p2)
+        cur.execute(f"select ST_Distance('{p1}','{p2}')")
         for i in cur:
             d=i[0]
         return d
