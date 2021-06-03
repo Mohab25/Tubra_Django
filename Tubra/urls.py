475fd1467fcdbf5cce0155032bcfd38f025ba836
Mohab <58084234+Mohab25@users.noreply.github.com>
2021-06-03 11:02:37 +0200

    Update urls.py

--- Tubra/urls.py
+++ Tubra/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('AerodromeFeatures/',include('Aerodrome_features.urls')),
     path('CAD/',include('CAD.urls')),
     path('Reports/',include('Documents.urls')),
+    path('spatial_analysis/',include('spatial_analysis.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
